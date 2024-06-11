e4f2a1502d515438dc3fd758a9aefb1412908cf9
unknown <11211086@student.itk.ac.id>
2024-06-11 23:44:43 +0800

    first commit

--- water-monitoring-dashboard/src/config/fireStore.js
+++ water-monitoring-dashboard/src/config/fireStore.js
@@ -3,15 +3,23 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const {
+  API_KEY: apiKey,
+  AUTH_DOMAIN: authDomain,
+  PROJECT_ID: projectId,
+  STORAGE_BUCKET: storageBucket,
+  MESSAGING_SENDER_ID: messagingSenderId,
+  APP_ID: appId,
+} = import.meta.env;
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCYY0LE_d8HIZpSTGxR0hj9M8i1eC80Pfk",
-  authDomain: "water-drone-patrol.firebaseapp.com",
-  projectId: "water-drone-patrol",
-  storageBucket: "water-drone-patrol.appspot.com",
-  messagingSenderId: "1054759755597",
-  appId: "1:1054759755597:web:03c28e17027003d4676104",
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
